697da7bd79d8a9084cb47f2fbf30d3253f3e9447
Nicolas Vera <nicolaas.vera@gmail.com>
2022-10-25 00:41:40 -0300

    creando el carrito de compras

---
+++ script.js
@@ -0,0 +1,18 @@
+let carritoCompras = Number(prompt("Bienvenido al carrito de compras. ¿Qué producto desea comprar? (1 - Playstation5 - 2 Xbox Series X - 3 Nintendo Switch"))
+
+if(carritoCompras == 1){
+
+    alert("Elegiste Playstation 5")
+}
+else if(carritoCompras ==2){
+
+    alert("Elegiste Xbox Series X")
+}
+else if(carritoCompras ==3){
+
+    alert("Elegiste Nintendo Switch")
+}
+else{
+
+    alert("El producto elegido no está en nuestro catálogo")
+}
