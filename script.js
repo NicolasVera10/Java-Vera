f25a3f466ffb3c842d5ea8e4ed5e671b9cff2fc8
Nicolas Vera <nicolaas.vera@gmail.com>
2022-11-12 19:51:32 -0300

    modificando el script

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const carrito = []
+let nombre = prompt("Ingrese su Nombre")
+alert("Bienvenido al Rincon del Gamer " + nombre)
 let articulo1 = Number(prompt("Bienvenido al carrito de compras. ¿Qué producto desea comprar? ( 1 - Playstation5 - 2 Xbox Series X - 3 Nintendo Switch)"))
+
+//const producto1 = new Producto("Playstation 5", 10000, 500);
+//const producto2 = new Producto("Xbox Series X", 15000, 300);
+//const producto3 = new Producto("Nintendo Switch", 8000, 50);
+
+
 
 if(articulo1 == 1){
 
@@ -19,3 +28,26 @@ else{
 
     alert("El producto elegido no está en nuestro catálogo")
 }
+
+class Producto{
+    constructor(nombre, precio, stock){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+        
+    }
+}
+
+
+
+productos.push(new Producto("Playstation 5", 10000, 500));
+productos.push(new Producto("xbox Series X", 15000, 300));
+productos.push(new Producto("Nintendo Switch", 8000, 50));
+
+const productos = [];
+
+
+
